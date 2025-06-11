9359e98084866759973f7bf205054786028f7a47
Dmitri <playcssv34@gmail.com>
2025-06-11 19:30:53 +0300

    create section works part 1

--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,33 @@
 import "../../node_modules/purecss/build/grids-min.css";
 import "../../node_modules/purecss/build/grids-responsive-min.css";
 import "../sass/style.scss";
+
+import Swiper from 'swiper';
+import { Navigation, Pagination } from 'swiper/modules';
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+
+const swiper = new Swiper('.swiper', {
+    slidesPerView: 1,
+    loop: true,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+      },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
+    breakpoints: {
+        // when window width is >= 320px
+        1200: {
+            slidesPerView: 3,
+            spaceBetween: 5,
+        },
+        1920: {
+            spaceBetween: 35,
+        }
+    },
+    modules: [Navigation, Pagination],
+});
